4c63c48f09745ca9cbde48006904677aba4641d4
ItsCamyx <96155623+ItsCamyx@users.noreply.github.com>
2022-05-18 20:42:51 -0300

    limpando o codigo e deixando ao de origem

--- src/controllers/eventosController.js
+++ src/controllers/eventosController.js
@@ -1,34 +1,25 @@
-import {VitrineController} from './produtoController.js'
+import { VitrineController } from "./produtoController.js";
 
+const arrayBotoesCategoria = [];
 
+function convertendoNodeListDeBotoes() {
+  const botaoCategoria = document.querySelectorAll(".navbar-lista__categoria");
 
-
-
-
- const arrayBotoesCategoria = []
-
- function convertendoNodeListDeBotoes(){
-    const botaoCategoria = document.querySelectorAll('.navbar-lista__categoria')
-
-    for(let i = 0; i < botaoCategoria.length; i++){
-        const botao = botaoCategoria[i]
-        arrayBotoesCategoria.push(botao)
-    }
- }
-
-convertendoNodeListDeBotoes()
-console.log(arrayBotoesCategoria)
-
-function adicionarEventosAosBotoes(){
-
-    for(let i = 0; i < arrayBotoesCategoria.length; i++){
-        const botao = arrayBotoesCategoria[i]
-        // console.log(botao)
-        botao.addEventListener('click', VitrineController.filtrarProdutos)
-    }
+  for (let i = 0; i < botaoCategoria.length; i++) {
+    const botao = botaoCategoria[i];
+    arrayBotoesCategoria.push(botao);
+  }
 }
 
-adicionarEventosAosBotoes()
+convertendoNodeListDeBotoes();
+console.log(arrayBotoesCategoria);
 
+function adicionarEventosAosBotoes() {
+  for (let i = 0; i < arrayBotoesCategoria.length; i++) {
+    const botao = arrayBotoesCategoria[i];
+    // console.log(botao)
+    botao.addEventListener("click", VitrineController.filtrarProdutos);
+  }
+}
 
-//  botaoCategoria.addEventListener('click', VitrineController.filtrarProdutos)
+adicionarEventosAosBotoes();
